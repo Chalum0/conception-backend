96a044edb8aa97b2da7b6740198a4f045b34949f
Raphael <raphael@Raphaels-MacBook-Air.local>
2025-11-02 16:52:48 +0100

    updated

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,4 @@
-import * as UserService from "../services/user.service.js";
+import UserService from "../services/index.service.js";
 
 export const registerUser = (_req, res) => {
   res.status(501).json({ message: "Not implemented yet." });
