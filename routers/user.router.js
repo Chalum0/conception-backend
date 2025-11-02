3561aaf0fbbfc51e6e22234d2548f20159c8f435
Debian <debian@vps-e862893c.vps.ovh.net>
2025-11-02 15:43:15 +0000

    updated

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,5 +1,9 @@
 import express from "express";
 
-const userRouter = express.Router()
+const userRouter = express.Router();
+
+userRouter.post("/auth/register", (_req, res) => {
+  res.status(501).json({ message: "Not implemented yet." });
+});
 
 export default userRouter;
